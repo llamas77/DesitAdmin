41c72c01242e6a31e86f5d1ee5928fb0bfb98670
Franco Llamas <llamas077@hotmail.com>
2018-03-26 13:53:11 -0300

    Proyecto en Maven y JavaFX

---
+++ src/main/java/com/desitsa/admin/controllers/MenuController.java
@@ -0,0 +1,16 @@
+package com.desitsa.admin.controllers;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MenuController implements Initializable {
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+
+}
